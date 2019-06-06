e62dd238dc01eddc4f5b7a569bfd8962adaaae6d
Tony Chen <lc465@cornell.edu>
2019-06-06 14:16:20 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Dash/>
+  </div>
+</template>
+
+<script>
+import Dash from './components/Dash.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Dash
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 40px;
+}
+</style>
---
+++ src/components/Dash.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="dash">
+  <h1>Frenzy</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Dash',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
